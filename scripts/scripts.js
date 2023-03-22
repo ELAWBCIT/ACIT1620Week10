bb988c653b81e43575a340758dd9f97c3ffcac60
Ernest Law <elawvc@gmail.com>
2023-03-22 12:19:44 -0700

    Phase 2 practicing JavaScript functions - Traditional and Arrow

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,15 +1,15 @@
-// Takes one parameter as a radius with a circle
-let user_input = prompt("Enter the radius of a circle");
+let user_input = prompt("Hello Please Enter Your Name");
 
-// This is the function that does the calculation
-function circle_area(radius){
-    let area = Math.PI * radius ** 2
-    return area
+// function welcome(name) {
+//     alert(`Welcome ${name}`);
+//     console.log(`Welcome ${name}!`);
+//     }
+
+// welcome(user_input)
+
+let welcome = (name) => { 
+    alert(`Welcome ${name}`);
+    console.log(`Welcome ${name}!`);
 }
 
-// Remember to put in a variable your outcome to be used later.
-let result = circle_area(user_input) 
-
-// Requested outcome from Neda. 
-alert(`The area of a circle with radius ${user_input} is ${result}.`)
-
+welcome(user_input)
